d91071b9e2916b34a0ec158280991031d93c7a4a
Asif Rasheed <b00073877@aus.edu>
2020-03-13 20:43:24 +0400

    Fixed Login Screen

--- setup.sh
+++ setup.sh
@@ -36,9 +36,12 @@ echo "::1    localhost" >> /etc/hosts
 echo "127.0.1.1    myarch" >> /etc/hosts
 
 # User Accounts
+clear
 echo "Setting up root user..."
 passwd
-read -p "Enter new username:" user
+
+$user = "user"
+read -p "Enter new username (default: user):" user
 useradd -m -G wheel $user
 passwd $user
 pacman -S sudo
@@ -60,7 +63,6 @@ grub-mkconfig -o /boot/grub/grub.cfg
 pacman -S xorg xorg-server plasma sddm
 systemctl enable sddm
 
-# Some test features
 tar xvf plasma-chili.tar -C /usr/share/sddm/themes
 
 cp /usr/lib/sddm/sddm.conf.d/default.conf /usr/lib/sddm/sddm.conf.d/default.bak
@@ -72,6 +74,7 @@ Current=plasma-chili
 systemctl enable NetworkManager.service
 
 # Finishing up
+clear
 echo "The Setup will install Firefox, Python3, Geany, GCC, Make and Terminal by default"
 read -p "Please enter all the extra package that you wish to install (default: None): " packages
 pacman -S firefox python3 geany gcc make qterminal $packages
--- test.sh
+++ test.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 read -p "Strictly for testing new features, please don't run if you're not a developer (Ctrl+c to quit, return to continue)" ret
 
-tar xvf plasma-chili.tar -C /usr/share/sddm/themes
-
-cp /usr/lib/sddm/sddm.conf.d/default.conf /usr/lib/sddm/sddm.conf.d/default.bak
-sed '33c\
-Current=plasma-chili
-' /usr/lib/sddm/sddm.conf.d/default.bak > /usr/lib/sddm/sddm.conf.d/default.conf
-
-cp /etc/sudoers /etc/sudoers.bak
-sed '82c\
-%wheel    ALL=(ALL)   ALL
-' /etc/sudoers.bak > /etc/sudoers
+#Please insert all the test code after this line
