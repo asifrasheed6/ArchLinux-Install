166f1e22080dfe2533c187dfbd1950002cdf26a6
Asif Rasheed <b00073877@aus.edu>
2020-03-13 14:08:09 +0400

    install File

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # ArchLinux-Install
-1. Create partition /dev/sda1: EFI partition and /dev/sda2: Root Location
-2. wifi-menu and connect to wifi
-3. pacman -Syy
-4. pacman -S git
-5. git clone https://github.com/asifrasheed6/ArchLinux-Install
-6. mv /ArchLinux-Install/* /
-7. sh install.sh
+1. wifi-menu and connect to wifi
+2. pacman -Syy
+3. pacman -S git
+4. git clone https://github.com/asifrasheed6/ArchLinux-Install
+5. mv /ArchLinux-Install/* /
+6. sh install.sh
 
 <b>Warning:</b> I wrote this script for my computer, might not work on yours. If you want to change the parition location, feel free to do that in the source code. Also the default desktop I'm installing is KDM plasma, feel free to change that in the source code before you install.
---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+echo "         ##
+                 ####
+                ######
+               ########
+              ##########
+             ############
+            ##############
+           ################
+          ##################
+         ####################
+        ######################
+       #########      #########
+      ##########      ##########
+     ###########      ###########
+    ##########          ##########
+   #######                  #######
+  ####                          ####
+ ###                              ###"
+echo "Install Arch Linux, written by Asif Rasheed"
+echo "Please enter your root directory: "
+read rdir
+echo "Please enter your efi directory: "
+read efi
+
+# Creating filesystem
+mkfs.fat -F32 $efi
+mkfs.ext4 $rdir
+
+# Selecting Mirror
+pacman -S reflector
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+reflector -c "US" -f 12 -l 10 -n 12 --save /etc/pacman.d/mirrorlist
+
+# Installing Arch Linux
+mount $rdir /mnt
+pacstrap /mnt base linux linux-firmware vim nano
+
+# Configuring install
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt
+timedatectl set-timezone Asia/Dubai
+
+# Setting Locale
+locale-gen
+echo LANG=en_GB.UTF-8 > /etc/locale.conf
+export LANG=en_GB.UTF-8
+
+# Network Configuration
+echo myarch > /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1	localhost" >> /etc/hosts
+echo "127.0.1.1	myarch" >> /etc/hosts
+
+# User Accounts
+echo "Setting up root user..."
+passwd
+echo "Enter username..."
+read user
+useradd -m $user
+passwd $user
+
+# Installing bootloader
+pacman -S grub efibootmgr
+mkdir /boot/efi
+mount $efi /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Desktop environment
+pacman -S xorg xorg-server plasma sddm
+systemctl enable sddm
+
+# Finishing up
+exit
+shutdown -r now
+
