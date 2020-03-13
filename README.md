a2145306c6af7d76589acf3301c2328434758a67
Asif Rasheed <b00073877@aus.edu>
2020-03-13 15:47:55 +0400

    BUG FIXES

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 4. git clone https://github.com/asifrasheed6/ArchLinux-Install
 5. cd ArchLinux-Install
 6. sh install.sh
+7. One install.sh finish running:
+	a. pacman -S git
+	b. git clone https://github.com/asifrasheed6/ArchLinux-Install
+	c. sh install2.sh
 
 <b>Warning:</b> I wrote this script for my computer, might not work on yours. If you want to change the parition location, feel free to do that in the source code. Also the default desktop I'm installing is KDM plasma, feel free to change that in the source code before you install.
--- install.sh
+++ install.sh
@@ -39,41 +39,6 @@ pacstrap /mnt base linux linux-firmware vim nano
 # Configuring install
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
-timedatectl set-timezone Asia/Dubai
-
-# Setting Locale
-locale-gen
-echo LANG=en_GB.UTF-8 > /etc/locale.conf
-export LANG=en_GB.UTF-8
-
-# Network Configuration
-echo myarch > /etc/hostname
-touch /etc/hosts
-echo "127.0.0.1	localhost" >> /etc/hosts
-echo "::1	localhost" >> /etc/hosts
-echo "127.0.1.1	myarch" >> /etc/hosts
-
-# User Accounts
-echo "Setting up root user..."
-passwd
-read -p "Enter username..." user
-useradd -m $user
-passwd $user
-
-# Installing bootloader
-pacman -S grub efibootmgr
-mkdir /boot/efi
-mount $efi /boot/efi
-grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Desktop environment
-pacman -S xorg xorg-server plasma sddm
-systemctl enable sddm
-
-# Finishing up
-pacman -S firefox python3 git geany gcc make sudo qterminal
 
 exit
 shutdown -r now
-
