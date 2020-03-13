ffdfced8b7cafd67c477006c85a8e45db02086da
Asif Rasheed <b00073877@aus.edu>
2020-03-13 17:20:38 +0400

    New Login Screen

--- setup.sh
+++ setup.sh
@@ -21,8 +21,6 @@ echo "
                      "
 echo "Setting up ArchLinux..."
 
-cd ..
-
 # Setting Locale
 timedatectl set-timezone Asia/Dubai # Default Location: Dubai
 locale-gen
@@ -52,8 +50,9 @@ grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Desktop environment setup
-pacman -S xorg xorg-server plasma sddm
-sudo tar -xzvf ~/Downloads/kde-plasma-chili.tar.gz -C /usr/share/sddm/themes
+pacman -S xorg xorg-server plasma sddm plymouth-theme-monoarch
+sudo tar -xzvf kde-plasma-chili.tar.gz -C /usr/share/sddm/themes
+rm -rf kde-plasma-chili.tar.gz
 cp /usr/lib/sddm/sddm.conf.d/default.conf /usr/lib/sddm/sddm.conf.d/default.bak
 sed '33c\
 Current=plasma-chili' /usr/lib/sddm/sddm.conf.d/default.bak > /usr/lib/sddm/sddm.conf.d/default.conf
@@ -67,4 +66,6 @@ echo "The Setup will install Firefox, Python3, Geany, GCC, Make and Terminal"
 read -p "Please enter all the extra package that you wish to install (default: None): " packages
 pacman -S firefox python3 geany gcc make sudo qterminal $packages
 
+cd ..
 echo "Setup Complete!, You may restart the machine!"
+
