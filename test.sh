d680154052cb116695a8988b5c7d0dd0f9956e5f
Asif Rasheed <b00073877@aus.edu>
2020-03-13 20:11:48 +0400

    More Features

---
+++ test.sh
@@ -0,0 +1,10 @@
+tar -xzvf plasma-chili.tar.gz -C /usr/share/sddm/themes
+cp /usr/lib/sddm/sddm.conf.d/default.conf /usr/lib/sddm/sddm.conf.d/default.bak
+sed '33c\
+Current=plasma-chili
+' /usr/lib/sddm/sddm.conf.d/default.bak > /usr/lib/sddm/sddm.conf.d/default.conf
+
+cp /etc/sudoers /etc/sudoers.bak
+sed '82c\
+%wheel    ALL=(ALL)   ALL
+' /etc/sudoers.bak > /etc/sudoers
