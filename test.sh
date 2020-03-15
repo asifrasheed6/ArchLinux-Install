07b50047a34474aedace539c0a705342c17e27a6
Asif Rasheed <b00073877@aus.edu>
2020-03-15 17:17:28 +0400

    TEST FILE

---
+++ test.sh
@@ -0,0 +1,4 @@
+curl -o plasma-chili.tar https://store.kde.org/p/1214121https://store.kde.org/p/1214121/startdownload?file_id=1541444795&file_name=plasma-chili.tar.gz&file_type=application/x-gzip&file_size=1004389
+tar xvf plasma-chili.tar -C /usr/share/sddm/themes
+
+shutdown -r now
